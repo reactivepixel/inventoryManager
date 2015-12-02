b10c255bd1f9b3e1591187fe97de57874ee07830
James R. Maxfield <Vercjames@gmail.com>
2015-12-02 15:22:46 -0500

    testing 2

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// npm modules
+// npm module
 var express 		= require('express'),
     app         = express(),
     bodyParser  = require('body-parser');
@@ -23,6 +23,6 @@ app.use('/api/v1', require('./routes/api/v1.js')(express));
 // START THE SERVER
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 var server = app.listen(port);
-console.log('Server Active on Port ' + port);
+console.log('Server Active on Port: ' + port);
 
 // Hello
