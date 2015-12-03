a7da4cf19cb3e18d41b3f05462f3d23176d943d4
= <=>
2015-12-03 14:47:54 -0500

    Webpack, gulpfile and starting point for ReachJS

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ require('dotenv').load();
 
 var express 	= require('express'),
     app         = express(),
-    bodyParser  = require('body-parser')
+    bodyParser  = require('body-parser'),
     env 		= process.env,
     Sequelize  	= require('sequelize');
 
 // Config
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-var port = process.env.PORT || 3000;
+var port = env.PORT || 3000;
 
 var sequelize = new Sequelize(env.DB_NAME, env.MYSQL_NAME, env.MYSQL_PASS, {
   host: 'localhost',
