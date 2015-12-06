51b707650e88ba6a373b892bbdc0379d06728c4c
Chapman <chapman@apextion.com>
2015-12-06 18:00:49 -0500

    fixed merge confl remaining with master

--- server/db.js
+++ server/db.js
@@ -1,172 +1,169 @@
 module.exports = function(){
-var express     = require('express'),
-    app         = express(),
-    mysql       = require('mysql'),
-    Sequelize   = require('sequelize');
+  var express     = require('express'),
+      app         = express(),
+      mysql       = require('mysql'),
+      Sequelize   = require('sequelize');
 
-// Config
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-module.exports = function(){
-var sequelize = new Sequelize(process.env.DB_NAME, process.env.MYSQL_NAME, process.env.MYSQL_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  port:process.env.DB_PORT,
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-});
+  // Config
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    var sequelize = new Sequelize(process.env.DB_NAME, process.env.MYSQL_NAME, process.env.MYSQL_PASS, {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      port:process.env.DB_PORT,
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      }
+    });
 
-// Checking connection status
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-var test = sequelize.authenticate().then(function(){
-    console.log("connected");
-  }).catch(function(err){
-    console.log("something goofed", err);
-  })
-  .done()
+    // Checking connection status
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    var test = sequelize.authenticate().then(function(){
+        console.log("connected");
+      }).catch(function(err){
+        console.log("something goofed", err);
+      })
+      .done()
 
-// Create Tables
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-var order = sequelize.define('orders', {
-  time_stamp: {
-    type: Sequelize.STRING,
-    createdAt: true
-  },
-  recipient: {
-    type: Sequelize.STRING,
-  },
+    // Create Tables
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    var order = sequelize.define('orders', {
+      time_stamp: {
+        type: Sequelize.STRING,
+        createdAt: true
+      },
+      recipient: {
+        type: Sequelize.STRING,
+      }
+    })
+
+    // Create Units Table
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    var unit = sequelize.define('units', {
+      sku: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      qty_on_hand: {
+        type: Sequelize.INTEGER,
+      },
+      trigger_qty: {
+        type: Sequelize.INTEGER,
+      },
+      replenish_qty: {
+        type: Sequelize.INTEGER,
+      }
+
+    })
+
+    // Create worker Table
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    var worker = sequelize.define('workers', {
+      qty_on_hand: {
+        type: Sequelize.INTEGER,
+      },
+      trigger_qty: {
+        type: Sequelize.INTEGER,
+      },
+      replenish_qty: {
+        type: Sequelize.INTEGER,
+      }
+
+    })
+
+    // Create Jobs Table
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    var job = sequelize.define('jobs', {
+      job_desc: {
+        type: Sequelize.STRING
+      },
+
+    })
+
+    // Create Pods Table
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    var pod = sequelize.define('pods', {
+      current_weight: {
+        type: Sequelize.INTEGER
+      },
+
+    })
+
+    // Create job_relations Table
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    var job_relation = sequelize.define('job_relation', {})
+
+    // Create status Table
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    var status = sequelize.define('status', {
+      status_type: {
+        type: Sequelize.STRING
+      }
+    })
+
+    // Create unit_description Table
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    var unit_description = sequelize.define('unit_description', {
+      sku:{
+        type:Sequelize.INTEGER,
+        primaryKey: true
+      },
+      description:{
+        type: Sequelize.STRING
+      },
+      weight:{
+        type:Sequelize.INTEGER
+      }
+    })
+
+    // Create unit_pods Table
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    var unit_pod = sequelize.define('unit_pods', {
+      sku:{
+        type:Sequelize.INTEGER
+      },
+      qty:{
+        type:Sequelize.INTEGER
+      }
+    })
+
+    // Table Relations
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    job_relation.belongsTo(order);
+    order.hasMany(job_relation);
+
+    job_relation.belongsTo(worker);
+    worker.hasMany(job_relation);
+
+    job_relation.belongsTo(job);
+    job.hasMany(job_relation);
+
+    job_relation.belongsTo(status);
+    status.hasMany(job_relation);
+
+    order.belongsTo(unit_pod);
+    unit_pod.hasMany(order);
+
+    order.belongsTo(status);
+    status.hasMany(order);
 
 
-});
+    // Creating all the tables in the proper orders
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    // Push all tables to database
+    sequelize.sync();
 
-// Create Units Table
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-var unit = sequelize.define('units', {
-  sku: {
-    type: Sequelize.STRING,
-    primaryKey: true,
-  },
-  qty_on_hand: {
-    type: Sequelize.INTEGER,
-  },
-  trigger_qty: {
-    type: Sequelize.INTEGER,
-  },
-  replenish_qty: {
-    type: Sequelize.INTEGER,
+      return {
+        connection: sequelize,
+        order: order,
+        unit: unit,
+        worker: worker,
+        job: job,
+        pod: pod,
+        job_relation: job_relation,
+        status: status,
+        unit_description: unit_description,
+        unit_pod: unit_pod
+      };
   }
-
-})
-
-// Create worker Table
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-var worker = sequelize.define('workers', {
-  qty_on_hand: {
-    type: Sequelize.INTEGER,
-  },
-  trigger_qty: {
-    type: Sequelize.INTEGER,
-  },
-  replenish_qty: {
-    type: Sequelize.INTEGER,
-  }
-
-})
-
-// Create Jobs Table
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-var job = sequelize.define('jobs', {
-  job_desc: {
-    type: Sequelize.STRING
-  },
-
-})
-
-// Create Pods Table
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-var pod = sequelize.define('pods', {
-  current_weight: {
-    type: Sequelize.INTEGER
-  },
-
-})
-
-// Create job_relations Table
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-var job_relation = sequelize.define('job_relation', {})
-
-// Create status Table
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-var status = sequelize.define('status', {
-  status_type: {
-    type: Sequelize.STRING
-  }
-})
-
-// Create unit_description Table
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-var unit_description = sequelize.define('unit_description', {
-  sku:{
-    type:Sequelize.INTEGER,
-    primaryKey: true
-  },
-  description:{
-    type: Sequelize.STRING
-  },
-  weight:{
-    type:Sequelize.INTEGER
-  }
-})
-
-// Create unit_pods Table
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-var unit_pod = sequelize.define('unit_pods', {
-  sku:{
-    type:Sequelize.INTEGER
-  },
-  qty:{
-    type:Sequelize.INTEGER
-  }
-})
-
-// Table Relations
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-job_relation.belongsTo(order);
-order.hasMany(job_relation);
-
-job_relation.belongsTo(worker);
-worker.hasMany(job_relation);
-
-job_relation.belongsTo(job);
-job.hasMany(job_relation);
-
-job_relation.belongsTo(status);
-status.hasMany(job_relation);
-
-order.belongsTo(unit_pod);
-unit_pod.hasMany(order);
-
-order.belongsTo(status);
-status.hasMany(order);
-
-
-// Creating all the tables in the proper orders
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-// Push all tables to database
-sequelize.sync();
-
-  return {
-    connection: sequelize,
-    order: order,
-    unit: unit,
-    worker: worker,
-    job: job,
-    pod: pod,
-    job_relation: job_relation,
-    status: status,
-    unit_description: unit_description,
-    unit_pod: unit_pod
-  }
-}
