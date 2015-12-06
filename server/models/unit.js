6297e0e9fe15fbef2ccb4bd6a85b5db05b3c5eb4
Brandy Bergh <brandymbergh@gmail.com>
2015-12-06 09:14:20 -0500

    AddOne for CRUD is workinggaaRemoveOne is almost there.

--- server/models/unit.js
+++ server/models/unit.js
@@ -27,12 +27,14 @@ var unit = sequelize.define('units', {
 _addOne = function(data, success, fail){
 unit.sync({force:true}).then(function(){
   unit.create({
-    sku: unit.sku,
-    qty_on_hand: unit.qty_on_hand,
-    trigger_qty:unit.trigger_qty,
-    replenish_qty:unit.replenish_qty
+    sku: data.sku,
+    qty_on_hand: data.qty_on_hand,
+    trigger_qty:data.trigger_qty,
+    replenish_qty:data.replenish_qty
   }).then(function (data){
-     data.save();
+     data.save().then(function(){
+      console.log("success")
+     });
     console.log("success")
   }).catch(function (err){
     console.log("error", err)
@@ -66,16 +68,16 @@ unit.sync({force:true}).then(function(){
   }
 
 
-  _remove = function (data, success, fail){
-  unit.find({where: {sku: unit.sku}}).then(function (err, data) {
+  _remove = function (data, idx, success, fail){
+  unit.find({where: {sku: data.sku}}).then(function (err, data) {
     console.log("remove hit", data);
       if (err) {
           console.log("Error: ", err);
       } else {
-          data.destroy({sku: unit.sku}).success(function (err, data) {
+          data.destroy({sku: data.sku}).success(function (err, data) {
             console.log("Successful removal: ", data);
               if(err){
-                  console.log(err);
+                  console.log("error 2: ", err);
               }else{
                   console.log('hits', data);
               }
@@ -84,8 +86,8 @@ unit.sync({force:true}).then(function(){
       console.log(data);
   });
 }
-_remove({sku:"220AbC"});
-_addOne({sku:"676GHY", qty_on_hand: 3, trigger_qty:4, replenish_qty:5});
+_remove({sku:"676GHY"});
+//_addOne({sku:"676GHY", qty_on_hand: 3, trigger_qty:4, replenish_qty:5});
   return{
     all: _findAll,
     findOne: _findOne,
