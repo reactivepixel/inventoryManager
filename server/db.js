c76cf03110a9df40245643c5a933ef9b1d53b3c5
jdcarroll <jdcarroll22@gmail.com>
2015-12-09 15:37:24 -0500

    added functions to library for modularity

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 module.exports = function(){
-  var express     = require('express'),
+  var express     = require('express');
   var app         = express();
   var mysql       = require('mysql');
   var Sequelize   = require('sequelize');
--- server/models/unit.js
+++ server/models/unit.js
@@ -3,20 +3,7 @@ module.exports = function (){
   var data = require('../../lib/sanitize.js');
   var Sequelize = require('sequelize');
   var sequelize = db.connection;
-
-  // TODO make a utility for more than just the API out of the ID Generator
-  var APIFunctions = {
-    idGenerator : function(){
-      function s4() {
-        // handles making unqiue characters in sets of 4
-        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-      }
-
-      // combine and return a string of random characters several.
-      // the first character MUST be a letter for id's to work. we've gone with "J"
-      return "j" + s4() + s4() +  s4() +  s4() +  s4() + s4() + s4() + s4();
-    }
-  };
+  var generator = require('../../lib/sanitize.js');
 
   // TODO Write a nice system wide defaultFail DB interaction Failure
   var defaultFail = function(err, doc){ console.log('err' + err + doc); }
@@ -63,7 +50,7 @@ module.exports = function (){
     payload = defaultSanitize(payload);
     // Parse payload to be applied to the defined properties
     unit.create({
-      sku: payload.sku || APIFunctions.idGenerator(),
+      sku: payload.sku || generator.idGenerator(),
       qty_on_hand: payload.qty_on_hand,
       trigger_qty: payload.trigger_qty,
       replenish_qty: payload.replenish_qty
