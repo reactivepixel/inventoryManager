80700e89775863ca7f96a02196348c2d0de9ff89
JoeyL626 <joeylivermore@Joeys-MacBook-Pro-5.local>
2015-12-14 23:48:19 -0500

    workers added ignore other branch with worker end points

---
+++ server/routes/api/v1/worker/create.js
@@ -0,0 +1,39 @@
+// Gravity Application API | Create a worker in the database
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+module.exports = function (express) {
+  var router = express.Router();
+  var worker = require('../../../../models/worker.js');
+
+  // /api/v1/worker/create
+  router.post('/create', function(req, res) {
+    var serverMessage = "Your worker is being created";
+    var serverResponse = "Your worker was created successfully";
+
+    // Request made from client
+    var clientWorkerPost = req.body;
+
+    // Example of data in JSON format
+    // worker.add({name: 'Muffin Man'}
+    worker.add({name: clientWorkerPost.name},
+    function(data){
+
+      // Server message of the request
+      console.log('A worker create request has been made');
+
+      res.json({
+        serverMessage: serverMessage,
+        serverResponse: serverResponse,
+      });
+    },
+
+    function(err){
+      res.json({
+        serverMessage: serverMessage,
+        serverResponse: "You've encountered an unknown error",
+        serverError: err
+      });
+    });
+  });
+
+  return router;
+};
