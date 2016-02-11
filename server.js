36ef5565a6b6aa6106e16234ffad110582345dbc
therealaldo <agonzalez2@fullsail.edu>
2016-02-10 23:58:29 -0500

    added uuid generator and timestamp

--- server.js
+++ server.js
@@ -8,21 +8,8 @@
 const express = require('express');
 const body_parser = require('body-parser');
 const app = express();
-const data =  { orders: { sku: 'a5296ab9-9eee-7ba0-0a79-b801594f2c91', quantity: 1, recipients: { name: 'John Doe', address:{ street: '3300 University Blvd', city: 'Winter Park', state: 'FL', zip: '32792'}, phone: '555-555-5555', email: 'jdoe@gmail.com' } } };
+const data =  { order: { sku: 'a5296ab9-9eee-7ba0-0a79-b801594f2c91', quantity: 1, recipients: { name: 'John Doe', address:{ street: '3300 University Blvd', city: 'Winter Park', state: 'FL', zip: '32792'}, phone: '555-555-5555', email: 'jdoe@gmail.com' } } };
 
-/*
-
-Possibly use this if you can figure out how to fix it
-
-let idGenerator = function() {
-  generateNew: function() {
-    function s4() {
-      return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    }
-    return  "j" + s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
-  }
-}
-*/
 function s4() {
   return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 }
@@ -31,11 +18,14 @@ function generateNew() {
   return  "j" + s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
 }
 
+data.orders.uuid = generateNew();
+var d = new Date();
+data.orders.timestamp = d;
 app.get('/', function(req, res) {
   res.json(data);
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
-    console.log("Listening on "+port+" "+generateNew());
+    console.log("Listening on "+port+"...");
 });
