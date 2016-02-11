4ef723ded9057f60d44ca4b07ade96a8a38d8f90
therealaldo <agonzalez2@fullsail.edu>
2016-02-10 23:09:51 -0500

    trying to fix server

--- server.js
+++ server.js
@@ -4,29 +4,29 @@
  *
  * Distributed under terms of the MIT license.
  */
-
+//"use strict";
 const express = require('express');
 const body_parser = require('body-parser');
 const app = express();
-const data =  { orders: { sku: 'a5296ab9-9eee-7ba0-0a79-b801594f2c91', quantity: 1, recipients: { name: 'John Doe', address:{ street: '3300 University Blvd', city: 'Winter Park', state: 'FL', zip: '32792'}, phone: '555-555-5555', email: 'jdoe@gmail.com' } } };                     
+const data =  { orders: { sku: 'a5296ab9-9eee-7ba0-0a79-b801594f2c91', quantity: 1, recipients: { name: 'John Doe', address:{ street: '3300 University Blvd', city: 'Winter Park', state: 'FL', zip: '32792'}, phone: '555-555-5555', email: 'jdoe@gmail.com' } } };
 
-var idGenerator = {
-    generateNew: function() {
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-        }
-        return  "j" + s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4()
+/*
+let idGenerator = function() {
+  generateNew: function() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     }
-};
-
+    return  "j" + s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
+  }
+}
+*/
 
 app.get('/', function(req, res) {
-    res.json(data);
+  res.json(data);
 });
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
-    console.log("Listening on "+port+" "+idGenerator.generateNew);
+    console.log("Listening on "+port+" ");
     return idGenerator;
-
 });
