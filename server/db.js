7f9f5571fb7093db6011060eeecb6f2a5ff8650c
therealaldo <agonzalez2@fullsail.edu>
2016-02-16 00:03:04 -0500

    fixing errors from pull request

--- server/db.js
+++ server/db.js
@@ -8,7 +8,7 @@ module.exports = function() {
 
   // Initializing sequelize
   const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     port: process.env.DB_PORT,
     pool: {
