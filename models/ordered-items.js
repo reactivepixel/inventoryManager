33a0e03ecc19bcbecf940d4641feabee7857a586
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2016-02-16 03:30:25 -0500

    inserts items into database

--- models/ordered-items.js
+++ models/ordered-items.js
@@ -6,4 +6,11 @@ module.exports = function() {
 
   // Defining orderedItems based on the table schema from the db.js
   let orderedItems = db.orderedItems;
+	
+	// Inserts items into the database
+	orderedItems.build({
+		sku: 'j098341jacjk0',
+		orderId: '1',
+		quantity: '4'
+	}).save()
 }();
