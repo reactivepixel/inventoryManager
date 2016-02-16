7170069d5daa3223799d0005ca93cf6c07a16e0b
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2016-02-15 23:15:08 -0500

    moved create table functions to db.js so you only have to run 1 file to create all the tables

--- models/units.js
+++ models/units.js
@@ -1,18 +1,7 @@
 'use strict';
-const db = require('../server/db.js')();
-const Sequelize = require('sequelize');
-const sequelize = db.connection;
-
-const units = sequelize.define('units', {
-    sku: {
-        type: Sequelize.STRING
-    },
-    name: {
-        type: Sequelize.STRING
-    },
-    description: {
-        type: Sequelize.STRING
-    }
-});
-
- sequelize.sync();
+module.exports = function() {
+  const db = require('../server/db.js')();
+  const Sequelize = require('sequelize');
+  const sequelize = db.connection;
+	const units = db.units;
+}
