27b89b8307fcee419448289d1a3ac4cdc80241b5
therealaldo <agonzalez2@fullsail.edu>
2016-02-18 14:13:52 -0500

    first attempt at order/create

--- routes/order.js
+++ routes/order.js
@@ -3,6 +3,7 @@ module.exports = function (express) {
 
   // Config
   const router = express.Router();
+  const order = require('../models/orders.js');
 
   // Include uuid generator and timestamp generator
   const uuid_generator = require('../server/uuid-generator.js');
@@ -20,7 +21,24 @@ module.exports = function (express) {
       data.uuid = uuid_generator.generateUUID();
       data.timestamp = timestamp.makeTimestamp();
       // ending the response and console logging the response data
-      console.log(data);
+      // console.log(data);
+      order.create({
+        orderId: data.uuid,
+        fullName: data.recipients.name,
+        streetAddress: data.recipients.address.street,
+        city: data.recipients.address.city,
+        state: data.recipients.address.state,
+        zip: data.recipients.address.zip,
+        phone: data.recipients.phone,
+        email: data.recipients.email,
+        timeStamp: data.timeStamp
+      }, function(data) {
+        console.log('An order has been created.');
+      }, function(error) {
+        console.log('You\'ve encountered an error.');
+      }
+      });
+
     });
 
   return router;
