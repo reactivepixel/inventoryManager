d1f9059c912d5f3b9b2b43d7e7ce90293a45b11a
Chris Fortin <cjfortin@fullsail.edu>
2016-02-25 00:13:02 -0500

    changed inventory model and units model

--- models/inventory.js
+++ models/inventory.js
@@ -1,11 +1,57 @@
 'use strict';
 module.exports = function() {
-  const db = require('../server/db.js');
-  const Sequelize = require('sequelize');
+  const db = require('../server/db.js')();
+  // const Sequelize = require('sequelize');
   const sequelize = db.connection;
 
-  // Defining inventory based on the table schema from the db.js
-  let inventory = db.inventory.create({
+  function _create(data, err, success) {
+    let payload = data;
 
-  });
+    db.inventory.create({
+      sku: payload.sku,
+      location: payload.location
+    })
+    .then(success)
+    .catch(err);
+  }
+
+  function _update(data, err, success) {
+    let payload = data;
+    db.inventory.find({where: {sku: payload.sku}})
+    .then(function(matchedOrder) {
+      matchedOrder.updateAttributes(data)
+      .then(success)
+      .catch(err)
+    })
+    .catch(err)
+  }
+
+  function _find(data, err, success) {
+    let payload = data;
+    db.inventory.find({where: {sku: payload.sku}})
+    .then(success)
+    .catch(err);
+  }
+
+  function _findAll(err, success) {
+    db.inventory.findAll()
+    .then(success)
+    .catch(err);
+  }
+
+  function _destroy(data, err, success) {
+    let payload = data;
+    db.inventory.destroy({where: {sku: payload.sku}})
+    .then(success)
+    .catch(err);
+  }
+
+  return {
+    create: _create,
+    update: _update,
+    find: _find,
+    findAll: _findAll,
+    destroy: _destroy
+  }
+
 }();
--- models/units.js
+++ models/units.js
@@ -1,9 +1,59 @@
 'use strict';
 module.exports = function() {
-  const db = require('../server/db.js');
-  const Sequelize = require('sequelize');
+  const db = require('../server/db.js')();
+  // const Sequelize = require('sequelize');
   const sequelize = db.connection;
 
-  // Defining units based on the table schema from the db.js
-  let units = db.units;
-}();
+  function _create(data, err, success) {
+    let payload = data;
+
+    db.units.create({
+      sku: payload.sku,
+      name: payload.name,
+      description: payload.description
+    })
+    .then(success)
+    .catch(err);
+  }
+
+  function _update(data, err, success) {
+    let payload = data;
+    db.units.find({where: {sku: payload.sku}})
+    .then(function(matchedOrder) {
+      matchedOrder.updateAttributes(data)
+      .then(success)
+      .catch(err);
+    })
+    .catch(err);
+  }
+
+  function _find(data, err, success) {
+    let payload = data;
+    db.units.find({where: {sku: payload.sku}})
+    .then(success)
+    .catch(err)
+  }
+
+  function _findAll(err, success) {
+    let payload = data;
+    db.units.findAll()
+    .then(success)
+    .catch(err)
+  }
+
+  function _destroy(data, err, success) {
+    let payload = data;
+    db.units.destroy({where: {orderId: payload.sku}})
+    .then(success)
+    .catch(err);
+  }
+
+  return {
+    create: _create,
+    update: _update,
+    find: _find,
+    findAll: _findAll,
+    destroy: _destroy
+  }
+
+}
